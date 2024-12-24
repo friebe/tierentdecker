e73ef08b12c23b53a56cb2b79533d410a5a7aaae
Jan Friebe <friebe@odas.biz>
2024-12-24 13:31:12 +0100

    implement feedback

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import confetti from 'canvas-confetti';
 import QuizCard from '../components/quiz/QuizCard.vue';
 import { animals } from '../data/animals';
@@ -8,6 +8,10 @@ import { shuffleArray, getRandomItem } from '../utils/quiz';
 const quizAnimals = ref(shuffleArray(animals).slice(0, 4));
 const targetAnimal = ref(getRandomItem(quizAnimals.value));
 const incorrectAttempts = ref<number[]>([]);
+
+onMounted(() => {
+  newQuiz();
+});
 
 const speakQuestion = (artikel: string, name: string) => {
   const speech = new SpeechSynthesisUtterance(`Wo ist ${artikel} ${name}?`);
@@ -22,6 +26,10 @@ const checkAnswer = (animalId: number) => {
       spread: 70,
       origin: { y: 0.6 }
     });
+    const speech = new SpeechSynthesisUtterance(`Richtig!!`);
+    speech.lang = 'de-DE';
+    window.speechSynthesis.speak(speech);
+
     setTimeout(newQuiz, 1500);
   } else {
     incorrectAttempts.value.push(animalId);
@@ -37,24 +45,23 @@ const newQuiz = () => {
 };
 
 const isCorrect = (animalId: number) => {
-  if (incorrectAttempts.value.includes(animalId)) return false;
+  if (incorrectAttempts.value.includes(animalId)) {
+    const speech = new SpeechSynthesisUtterance(`Leider falsch`);
+    speech.lang = 'de-DE';
+    window.speechSynthesis.speak(speech);
+
+    return false;
+  }
+
   return null;
 };
 </script>
 
 <template>
-  <div class="fixed inset-0 bg-background-color flex flex-col items-center justify-center p-4">
-    <div class="text-center mb-4">
-      <h1 class="text-3xl md:text-4xl font-bold text-primary-color">
-        Wo ist {{ targetAnimal.artikel }} {{ targetAnimal.name }}?
-      </h1>
-    </div>
-
-    <div class="w-full h-[70vh] max-w-5xl mx-auto">
-      <div class="grid grid-cols-2 gap-4 h-full">
-        <QuizCard v-for="animal in quizAnimals" :key="animal.id" :animal="animal" :is-correct="isCorrect(animal.id)"
-          @select="checkAnswer(animal.id)" />
-      </div>
+  <div class="fixed inset-0 bg-background-color">
+    <div class="w-full h-full grid grid-cols-2 grid-rows-2 gap-2 p-2">
+      <QuizCard v-for="animal in quizAnimals" :key="animal.id" :animal="animal" :is-correct="isCorrect(animal.id)"
+        @select="checkAnswer(animal.id)" />
     </div>
   </div>
 </template>
