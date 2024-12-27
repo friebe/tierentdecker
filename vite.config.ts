2819bbfb8f182e3aeaa5625e232b1e1b493afec7
Jan Friebe <friebe@odas.biz>
2024-12-27 19:51:00 +0100

    add support for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+
+      manifest: {
+        name: 'vite-project',
+        short_name: 'vite-project',
+        description: 'vite-project',
+        theme_color: '#ffffff',
+      },
+    })
+  ],
+  preview: {
+    https: true, // Aktiviert HTTPS im Preview-Modus
+    port: 3000,  // Wähle deinen Port
+  },
 })
