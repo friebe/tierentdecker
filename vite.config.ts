03c7c36b1089f8eeb2ab2cced3913891eb583426
Jan Friebe <friebe@odas.biz>
2024-12-27 20:58:04 +0100

    remove cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
 
       manifest: {
         start_url: '/',
-        display: 'fullscreen',
+        display: 'standalone',
         name: 'tierentdecker',
         short_name: 'tierentdecker',
         description: 'tierentdecker',
