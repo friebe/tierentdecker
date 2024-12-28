f84c6a471f07e4d00f503b7d0dee5b3c5919db08
Jan Friebe <friebe@odas.biz>
2024-12-28 20:38:36 +0100

    fix build

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import confetti from 'canvas-confetti';
 import QuizCard from '../components/quiz/QuizCard.vue';
 import { animals } from '../data/animals';
