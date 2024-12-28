af67e9864139fa960bc0e7c7427a50f79cd99a65
Jan Friebe <friebe@odas.biz>
2024-12-28 19:06:26 +0100

    include pwa icons into manifest

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,31 @@ export default defineConfig({
       injectRegister: 'auto',
 
       manifest: {
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+
         theme_color: "#f69435",
         background_color: "#f69435",
         display: "standalone",
